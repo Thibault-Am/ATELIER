268430d457741b2fe8ddb9bb58a5c9abe48140df
AMAGAT Thibault <amagat.thibault@gmail.com>
2021-11-02 11:53:25 +0100

    ajout de User.php

---
+++ src/app/User.php
@@ -0,0 +1,9 @@
+<?php
+namespace app\model;
+class User extends \Illuminate\Database\Eloquent\Model {
+
+protected $table      = 'user';  /* le nom de la table */
+protected $primaryKey = 'id';     /* le nom de la clé primaire */
+public    $timestamps = false;    /* si vrai la table doit contenir
+                                     les deux colonnes updated_at*/
+}
