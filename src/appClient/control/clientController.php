08023be978171ea422d7c9e3a1250f05c1c0f5fd
Thibault-Am <65908510+Thibault-Am@users.noreply.github.com>
2021-11-04 18:51:26 +0100

    Update clientController.php

--- src/appClient/control/clientController.php
+++ src/appClient/control/clientController.php
@@ -57,10 +57,67 @@ class clientController extends \mf\control\AbstractController {
         
     }
     public function viewPanier(){
-        $vues = new \appClient\view\ClientView(null);
+       
+            
+        $commande=\appClient\model\commande::where('Nom_client','=',$_SESSION['client_name']);
+        $lignes=$commande->get();
+        $vues = new \appClient\view\ClientView($lignes);
         return $vues->render('Panier');
+        
+        
+        
     }
-    public function setClient(){
-        echo $_GET['name'];
+    public function setPanier(){
+        $_SESSION['panier']=[];
+        $router = new \mf\router\Router();
+        header("Location: ".$router->urlFor('panier'));
     }
+    public function addPanier(){
+        if(empty($_SESSION['panier'])){
+            $this->setPanier();
+            $id_produit=$_GET['id_produit'];
+            $quantite=$_GET['quantite'];
+            array_push($_SESSION['panier'],[$id_produit=>$quantite]);
+            $router = new \mf\router\Router();
+            header("Location: ".$router->urlFor('panier'));
+        }else{
+            $id_produit=$_GET['id_produit'];
+            $quantite=$_GET['quantite'];
+            array_push($_SESSION['panier'],[$id_produit=>$quantite]);
+            $router = new \mf\router\Router();
+            header("Location: ".$router->urlFor('panier'));
+        }
+    }
+
+    public function validationPanier(){
+            $nom=$_GET['nom'];
+            $montant=$_GET['montant'];
+            $mail=$_GET['mail'];
+            $tel=$_GET['tel'];
+            $commande = new \appClient\model\Commande;
+            $commande->Nom_client=$nom;
+            $commande->Mail_client=$mail;
+            $commande->Tel_client=$tel;
+            $commande->Etat="en_cours";
+            $commande->Montant=$montant;
+            $commande->save();
+            $les_commandes = \appClient\model\Commande::orderBy('id','desc');
+            $la_commande=$les_commandes->first();
+            
+            foreach($_SESSION['panier'] as $tab_produit){
+                foreach($tab_produit as $id_produit=>$quant){
+                    $quantite = new \appClient\model\Quantite;
+                    $quantite->PRODUIT_ID=$id_produit;
+                    $quantite->COMMANDE_ID=$la_commande->id;
+                    $quantite->Quantite=$quant;
+                    $quantite->save();
+                }
+            }
+     
+            unset($_SESSION['panier']);
+            $router = new \mf\router\Router();
+            header("Location: ".$router->urlFor('categorie'));
+            
+    }
+
 }
